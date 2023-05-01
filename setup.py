bb8c44047f553666b6d3afbce9a8aebc2582041f
Karina <karina.gonzalezespinoza292@t-mobile.com>
2023-04-30 20:01:45 -0700

    changes

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     description = fh.read()
   
 setuptools.setup(
-    name="trident-models-package",
-    version="0.0.1",
+    name="trident_models_package",
+    version="0.0.2",
     author="Karina",
     author_email="karina.ge@slalom.com",
-    packages=["trident-models-package"],
+    packages=["trident_models_package"],
     description="ORM Mmodels",
     long_description=description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gituser/test-tackage",
+    url="https://gitlab.com/karina.ge-slalom/trident-models",
     license='MIT',
     python_requires='>=3.8',
     install_requires=[]
